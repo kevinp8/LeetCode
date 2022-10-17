e4baf85d2cab4712ff18e6e15285d8ec951f2c54
Kevin <kevdev.yr6re@slmail.me>
2022-10-17 00:41:30 -0700

    added kata

--- Product of Array Except Self/script.js
+++ Product of Array Except Self/script.js
@@ -1,17 +1,18 @@
-/**
- * @param {number[]} nums
- * @return {number[]}
- */
-
 var productExceptSelf = function(nums) {
-    let newNums = []
-    for (let i=0; i<nums.length; i++) {
-        let product = 1
-        for (let j=0; j<nums.length; j++) {
-            if (j != i) product *= nums[j]
-        }
-        newNums.push(product)
-    }
-    return newNums
-};
+    let products = []
+    for (let i=0; i<nums.length; i++) products.push(1)
 
+    let temp = 1;
+    for (i = 0; i < nums.length; i++) {
+        products[i] = temp;
+        temp *= nums[i];
+    }
+ 
+    temp = 1;
+    for (i = nums.length - 1; i >= 0; i--) {
+        products[i] *= temp;
+        temp *= nums[i];
+    }
+ 
+    return products;
+};
