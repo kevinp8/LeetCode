852854e2d6da2548462c6687baac70f1c5f9b120
Kevin <kevdev.yr6re@slmail.me>
2022-11-15 00:19:54 -0800

    python solution

---
+++ Top K Frequent Elements/script.py
@@ -0,0 +1,6 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        for i in nums:
+            count[i] = 1 + count.get(i, 0)
+        return sorted(list(count.keys()), key=lambda a : count[a])[-k:]
